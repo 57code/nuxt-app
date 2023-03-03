1b9ef708113493377c690831ce0c75752106bb14
57code <yt0379@sohu.com>
2023-03-03 08:51:14 +0800

    1802-uselayers

---
+++ base/nuxt.config.ts
@@ -0,0 +1,10 @@
+export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Extending configs by layers',
+      meta: [
+        { name: 'description', content: 'I am using the extends feature in nuxt3!' }
+      ],
+    }
+  }
+})
