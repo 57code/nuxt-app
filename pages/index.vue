536a061541f965dc2efc692f103e0c6c64895d2f
josephxia <15906475@qq.com>
2023-05-09 11:26:48 +0800

    feat: 增加轮播图嵌入图片

--- pages/index.vue
+++ pages/index.vue
@@ -5,10 +5,10 @@ useHead({
 
 const { data, error } = await useFetch('/api/indexdata')
 const slides = [
-  { label: '1', bgColor: 'cadetblue' },
-  { label: '2', bgColor: 'cornflowerblue' },
-  { label: '3', bgColor: 'blueviolet' },
-  { label: '4', bgColor: 'brown' },
+  { label: '1', bgColor: 'cadetblue' ,src : "./Vue3组件库实战.png"},
+  { label: '2', bgColor: 'cornflowerblue',src : "./Vue3组件库实战.png" },
+  { label: '3', bgColor: 'blueviolet',src : "./Vue3组件库实战.png" },
+  { label: '4', bgColor: 'brown',src : "./Vue3组件库实战.png" },
 ]
 if (process.server && error.value)
   showError('获取数据失败！')
@@ -17,12 +17,16 @@ if (process.server && error.value)
 <template>
   <!-- banner -->
   <n-carousel show-arrow class="mb-6">
+    <!-- <n-carousel-item v-for="(item, index) in slides" :key="index">
+        <img :src="item.src" alt="">
+    </n-carousel-item> -->
     <div
       v-for="item in slides" :key="item.label"
       class="text-white w-full h-[150px] lg:h-[400px] rounded cursor-pointer text-center leading-[400px]"
       :style="{ backgroundColor: item.bgColor }"
     >
       {{ item.label }}
+      <!-- <img src="../public/Vue3组件库实战.png" /> -->
     </div>
   </n-carousel>
   <ProdList
